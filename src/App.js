7c5037076c56097b941a5a446affed6ff975d4b5
Chad <chad.m.carlson@gmail.com>
2020-09-08 13:22:38 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [response, setResponse] = useState("");
+
+  const apiKey = "QIm9uFcnyGGhMhiOCiJrvEP8yWvJQH2f";
+  const locationId = "1743280";
+
+  const logIn = (e) => {
+    axios
+      .get(
+        `https://api.honeywell.com/oauth2/authorize?response_type=code&client_id=${apiKey}&redirect_uri=http://localhost:3000/`
+      )
+      .then((res) => console.log(res))
+      .catch((res) => console.log(res));
+  };
+  const handleClick = (e) => {
+    axios
+      .get(
+        `https://api.honeywell.com/v2/devices/apikey=${apiKey}$locationid=${locationId}`
+      )
+      .then((res) => console.log(res))
+      .catch((res) => console.log(res));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Thermostat</h1>
+      <button onClick={() => logIn()}>oauth login</button>
+      <button onClick={() => handleClick()}>click me</button>
+    </>
   );
-}
+};
 
 export default App;

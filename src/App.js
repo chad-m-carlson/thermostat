ca83df4e47083ed0e5b2a71b42a9dc6e17f3f8cd
Chad <chad.m.carlson@gmail.com>
2020-09-08 13:46:11 -0600

    api

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 const App = () => {
   const [response, setResponse] = useState("");
 
-  const apiKey = "QIm9uFcnyGGhMhiOCiJrvEP8yWvJQH2f";
+  const apiKey = process.env.REACT_APP_HONEYWELL;
   const locationId = "1743280";
 
   const logIn = (e) => {
     axios
       .get(
-        `https://api.honeywell.com/oauth2/authorize?response_type=code&client_id=${apiKey}&redirect_uri=http://localhost:3000/`
+        `https://api.honeywell.com/oauth2/authorize?response_type=code&client_id=${process.env.REACT_APP_HONEYWELL_KEY}&redirect_uri=http://localhost:3000/`
       )
       .then((res) => console.log(res))
       .catch((res) => console.log(res));
@@ -28,6 +28,8 @@ const App = () => {
   return (
     <>
       <h1>Thermostat</h1>
+      <p>you are running {process.env.NODE_ENV}</p>
+      <p>what is this {process.env.REACT_APP_HONEYWELL_KEY}</p>
       <button onClick={() => logIn()}>oauth login</button>
       <button onClick={() => handleClick()}>click me</button>
     </>
